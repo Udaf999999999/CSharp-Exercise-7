319c72b615de638572ee7870e4405ece3b3a412f
Udaf999999999 <udaf999999999@yandex.ru>
2024-12-10 07:21:34 +0700

    Add class Food and add inheritance's classes

--- OOPConsoleApp/Program.cs
+++ OOPConsoleApp/Program.cs
@@ -2,6 +2,65 @@
 {
     internal class Program
     {
+        class Employee
+        {
+            public string Name;
+            public int Age;
+            public int Salary;
+        }
+        class ProjectManager : Employee
+        {
+            private string ProjectName;
+        }
+        class Developer : Employee
+        {
+            private string ProgrammingLanguage;
+        }
+        class Food
+        {
+
+        }
+        class Vegetable : Food
+        {
+
+        }
+        class Fruit : Food
+        {
+
+        }
+        class Apple : Fruit
+        {
+
+        }
+        class Banana : Fruit
+        {
+
+        }
+        class Pear : Fruit
+        {
+
+        }
+        class Potato : Vegetable
+        {
+
+        }
+        class Carrot : Vegetable
+        {
+
+        }
+        class Obj
+        {
+            private string name;
+            private string owner;
+            private int length;
+            private int count;
+
+            public Obj(string name, string ownerName, int objLength, int count)
+            {
+                // ...
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
